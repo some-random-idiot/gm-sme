76021e8da3e4751c20d01c36f6e44be4d75bcf6d
some-random-idiot <some_random_idiot@outlook.com>
2025-07-26 15:07:48 +0700

    Hook ordering test on serverside emit sound hook.

--- lua/sme_modules/server/sme_emit_sv.lua
+++ lua/sme_modules/server/sme_emit_sv.lua
@@ -2,17 +2,28 @@ util.AddNetworkString("SMENetworkSound")
 
 local muffle = GetConVar("sme_active")
 
-hook.Add("EntityEmitSound", "SMEMuffler",  function(sndData)
+-- The emit sound hook has to be at the bottom of the stack to let other hooks modify sounds.
+local exclamationCount = 1
+local emitSoundHookTable = hook.GetTable()["EntityEmitSound"]
+
+if emitSoundHookTable then
+    local emitSoundHooks = table.GetKeys(emitSoundHookTable)
+    local _, replaceCount = string.gsub(emitSoundHooks[1], "!", "!")
+
+    exclamationCount = replaceCount
+end
+
+local exclamations = string.rep("!", exclamationCount + 1)
+
+hook.Add("EntityEmitSound",  exclamations .. "SMEMuffler",  function(sndData)
     if not muffle:GetBool() then return end
 
     if IsValid(sndData.Entity) and not sndData.Pos then
         -- This should help when the networking is too slow to network the entity in time before its removal.
         sndData.Pos = sndData.Entity:GetPos()
     end
-    
+
     net.Start("SMENetworkSound", true)
     net.WriteTable(sndData)
     net.SendPAS(sndData.Pos and sndData.Pos or IsValid(sndData.Entity) and sndData.Entity:GetPos() or vector_origin)
-
-    return false
 end)
