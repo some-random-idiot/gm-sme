d8a6ae04b6a449751c7193bbb05df8232cc40798
some-random-idiot <some_random_idiot@outlook.com>
2025-08-30 14:11:05 +0700

    Optimization.

--- lua/autorun/client/sme_cl.lua
+++ lua/autorun/client/sme_cl.lua
@@ -7,6 +7,9 @@ end
 hook.Add("EntityRemoved", "SMEEntRemoved", function(ent, fullUpdate)
     if fullUpdate then return end
 
+    -- Apparently emitting a null sound is kinda laggy, so let's only do it for NPCs. CHAN_VOICE should only be used by them anyways.
+    if not (ent:IsNPC() or ent:IsNextBot()) then return end
+
     -- CHAN_VOICE sounds seems to be able to exist without a parent entity so we need to stop it here.
     ent:EmitSound("common/null.wav", 75, 100, 1, CHAN_VOICE, SND_STOP_LOOPING)
 end)
